6c00c6afa8eee0c4146fd910480cdea9db824786
Hashane Sivanesan <101983689+Hashane0302@users.noreply.github.com>
2023-02-11 16:32:39 +0530

    Added all the related file for the project

---
+++ js/Index.js
@@ -0,0 +1,17 @@
+
+var menuList = document.getElementById("menuList");
+
+menuList.style.maxHeight = "Opx";
+
+function togglemenu(){
+
+  if(menuList.style.maxHeight=="0px") {
+
+      menuList.style.maxHeight="130px";
+
+     }
+     else
+     {
+        menuList.style.maxHeight="0px";
+     }
+}
